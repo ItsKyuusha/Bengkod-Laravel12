574e7785dfdf5cc1c051cc5fc1e8bc6f2b0f77a8
Kyuusha <daaddimaki04@gmail.com>
2025-04-07 21:48:26 +0700

    bengkod

---
+++ database/migrations/2025_04_07_125303_create_jadwal_periksas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('jadwal_periksas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pasien_id')->constrained('pasiens')->onDelete('cascade');
+            $table->foreignId('dokter_id')->constrained('users')->onDelete('cascade');
+            $table->date('tanggal');
+            $table->time('jam');
+            $table->enum('status', ['menunggu', 'selesai'])->default('menunggu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jadwal_periksas');
+    }
+};
